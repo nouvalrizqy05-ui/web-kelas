c5f953d7d20a787c0037ebdfbaaba38877d9dd8b
nouvalrizqy05-ui <nouvalrizqy05@gmail.com>
2026-01-20 13:21:02 +0700

    Update Gallery.jsx

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -66,7 +66,7 @@ const Carousel = () => {
   return (
     <>
       <div className="text-white opacity-60 text-base font-semibold mb-4 mx-[10%] mt-10 lg:text-center lg:text-3xl lg:mb-8" id="Gallery">
-        Class Gallery
+        Dignified Gallery
       </div>
 
       <div id="Carousel" className="px-4">
